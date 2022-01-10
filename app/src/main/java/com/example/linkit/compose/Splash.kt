58b48bca2e55eeeb78184e54f667d2c10a54c9d0
Won Suk <daily142857@gmail.com>
2022-01-11 00:56:32 +0900

    Splash 화면 구현

--- app/src/main/java/com/example/linkit/compose/Splash.kt
+++ app/src/main/java/com/example/linkit/compose/Splash.kt
@@ -1,36 +1,66 @@
 package com.example.linkit.compose
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.example.linkit.ui.theme.CustomTypo
+import com.example.linkit.ui.theme.roboto
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen() {
+fun SplashScreen(navController: NavHostController) {
+    var startAnimation by remember { mutableStateOf(false) }
+    val alphaAnim = animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 3000
+        )
+    )
+
+    LaunchedEffect(key1 = true) {
+        startAnimation = true
+        delay(3500)
+        navController.popBackStack()
+        navController.navigate(Screen.Home.route)
+    }
+    Splash(alpha = alphaAnim.value)
+}
+
+@Composable
+fun Splash(alpha: Float) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .background(Color.Black)
             .fillMaxSize()
+            .alpha(alpha = alpha)
     ) {
         Text(
             text = "LinkIt",
             color = Color.White,
-            fontFamily
-            fontWeight = FontWeight.ExtraBold,
-            fontSize = 60.sp,
-            modifier = Modifier.padding(top = 100.dp)
+            style = CustomTypo.logo,
+            modifier = Modifier
+                .padding(top = 140.dp)
+                .height(85.dp)
         )
         Text(
             text = "link repository",
-            color = Color.White
+            color = Color.White,
+            style = MaterialTheme.typography.subtitle1,
         )
     }
 }
