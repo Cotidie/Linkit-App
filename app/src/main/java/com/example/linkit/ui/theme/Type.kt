9224edc0c89d11246e45aca7c33a92e286fca6a2
Won Suk <daily142857@gmail.com>
2022-01-11 00:56:03 +0900

    폰트 글꼴 추가 (Roboto)

--- app/src/main/java/com/example/linkit/ui/theme/Type.kt
+++ app/src/main/java/com/example/linkit/ui/theme/Type.kt
@@ -1,16 +1,74 @@
 package com.example.linkit.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.linkit.R
+
+val roboto = FontFamily(
+    Font(
+        resId = R.font.roboto_black,
+        weight = FontWeight.Black
+    ),
+    Font(
+        resId = R.font.roboto_black_italic,
+        weight = FontWeight.Black,
+        style = FontStyle.Italic
+    ),
+    Font(
+        resId = R.font.roboto_bold,
+        weight = FontWeight.Bold,
+    ),
+    Font(
+        resId = R.font.roboto_bold_italic,
+        weight = FontWeight.Bold,
+        style = FontStyle.Italic
+    ),
+    Font(
+        resId = R.font.roboto_italic,
+        weight = FontWeight.Normal,
+        style = FontStyle.Italic
+    ),
+    Font(
+        resId = R.font.roboto_light,
+        weight = FontWeight.Light,
+    ),
+    Font(
+        resId = R.font.roboto_light_italic,
+        weight = FontWeight.Light,
+        style = FontStyle.Italic
+    ),
+    Font(
+        resId = R.font.roboto_regular,
+        weight = FontWeight.Normal,
+    ),
+    Font(
+        resId = R.font.roboto_thin,
+        weight = FontWeight.Thin,
+    ),
+    Font(
+        resId = R.font.roboto_thin_italic,
+        weight = FontWeight.Thin,
+        style = FontStyle.Italic
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = roboto,
         fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = roboto,
+        fontWeight = FontWeight.Light,
         fontSize = 16.sp
     )
     /* Other default text styles to override
@@ -26,3 +84,12 @@ val Typography = Typography(
     )
     */
 )
+
+object CustomTypo {
+    val logo = TextStyle(
+        fontFamily = roboto,
+        fontWeight = FontWeight.Bold,
+        fontSize = 70.sp,
+        lineHeight = 0.sp
+    )
+}
