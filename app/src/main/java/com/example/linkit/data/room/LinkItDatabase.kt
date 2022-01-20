3ce715bc14472ccd2169de6a3f691e91da45458d
Won Suk <daily142857@gmail.com>
2022-01-21 00:44:41 +0900

    feat: Link 전용 Database, DAO 정의

--- app/src/main/java/com/example/linkit/data/room/LinkItDatabase.kt
+++ app/src/main/java/com/example/linkit/data/room/LinkItDatabase.kt
@@ -1,4 +1,11 @@
 package com.example.linkit.data.room
 
-class LinkItDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.linkit.data.room.dao.LinkDao
+import com.example.linkit.data.room.entity.Link
+
+@Database(entities = [Link::class], version = 1, exportSchema = true)
+abstract class LinkItDatabase : RoomDatabase() {
+    abstract fun linkDao(): LinkDao
 }
--- app/src/main/java/com/example/linkit/data/room/dao/LinkDao.kt
+++ app/src/main/java/com/example/linkit/data/room/dao/LinkDao.kt
@@ -1,4 +1,32 @@
-package com.example.linkit.data.room
+package com.example.linkit.data.room.dao
 
+import androidx.room.*
+import com.example.linkit.data.room.entity.Link
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Data Access Object. 데이터베이스에 쿼리를 날린다.
+ */
+
+@Dao
 interface LinkDao {
+    // Flow는 객체만 생성되고, SELECT 작업은 .collect에서 이루어진다.
+    // 객체 생성은 변수 선언과 같으므로 코루틴으로 작업하지 않아도 된다.
+    @Query("SELECT * from link_table")
+    fun getLinks(): Flow<List<Link>>
+
+    @Query("SELECT * from link_table where id = :id")
+    suspend fun getLinkById(id: Long): Link
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(link: Link)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(link: Link)
+
+    @Query("DELETE FROM link_table")
+    suspend fun delete()
+
+    @Delete
+    suspend fun deleteLink(link: Link)
 }
