050f407450ac2634f8ec30d96c6f595c17977f06
Won Suk <daily142857@gmail.com>
2022-01-30 02:59:11 +0900

    feat: LinkCard 초안 완성

--- app/src/main/java/com/example/linkit/presentation/component/CustomChip.kt
+++ app/src/main/java/com/example/linkit/presentation/component/CustomChip.kt
@@ -1,6 +1,5 @@
 package com.example.linkit.presentation.component
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -9,7 +8,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun CustomChip(
@@ -25,7 +23,7 @@ fun CustomChip(
             modifier = textModifier,
             text = text,
             textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.subtitle2
+            style = MaterialTheme.typography.subtitle1
         )
     }
 }
--- app/src/main/java/com/example/linkit/presentation/component/LinkCard.kt
+++ app/src/main/java/com/example/linkit/presentation/component/LinkCard.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForwardIos
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -27,14 +29,14 @@ fun LinkCard(
 ) {
     Row(
         modifier = modifier
-            .height(50.dp)
+            .clip(RoundedCornerShape(13.dp))
             .background(Color.White)
-            .clip(RoundedCornerShape(8.dp))
+            .padding(start = 15.dp, top = 12.dp, bottom = 12.dp)
     ) {
         Image(
             modifier = Modifier
-                .padding(end=8.dp)
-                .size(50.dp)
+                .padding(end=15.dp)
+                .size(55.dp)
                 .clip(RoundedCornerShape(8.dp)),
             bitmap = link.image.asImageBitmap(),
             contentDescription = null,
@@ -48,19 +50,26 @@ fun LinkCard(
             Text(
                 modifier = Modifier.weight(1f),
                 text = link.url,
+                style = MaterialTheme.typography.subtitle1,
                 maxLines = 1
             )
             // 넘치는 경우 처리
             Row(
-                modifier = Modifier.weight(1f)
+                modifier = Modifier.weight(1f),
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 for (tag in link.tags) {
-                    CustomChip(text = "# $tag")
+                    CustomChip(
+                        modifier = Modifier.padding(end = 8.dp),
+                        textModifier = Modifier.padding(horizontal = 3.dp),
+                        text = "# $tag")
                 }
             }
         }
         Icon(
-            modifier = Modifier.fillMaxHeight(),
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(end=8.dp),
             imageVector = Icons.Filled.ArrowForwardIos,
             contentDescription = null
         )
@@ -79,5 +88,7 @@ fun PreviewLinkCard() {
         listOf("유명", "네이버", "검색")
     )
     
-    LinkCard(link = link)
+    LinkCard(
+        modifier = Modifier.height(80.dp),
+        link = link)
 }
