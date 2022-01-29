d9ed7e6d0256d526bc788ae22124e6a42e8f55b1
Won Suk <daily142857@gmail.com>
2022-01-30 03:01:07 +0900

    feat: 홈 화면에 서치바 적용

--- app/src/main/java/com/example/linkit/presentation/component/AppBarHome.kt
+++ app/src/main/java/com/example/linkit/presentation/component/AppBarHome.kt
@@ -1,5 +1,6 @@
 package com.example.linkit.presentation.component
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -8,13 +9,43 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.linkit.constant.UIConstants
+import com.example.linkit.enums.SearchBarState
+import com.example.linkit.enums.SearchBarState.*
 import com.example.linkit.ui.theme.LinkItTheme
+
+@Composable
+fun AppBarHome() {
+    var searchBarState by remember { mutableStateOf(CLOSED) }
+    var text by remember { mutableStateOf("") }
+
+    when (searchBarState) {
+        CLOSED -> {
+            AppBarHomeDefault(
+                onSearchClicked = { searchBarState = OPENED }
+            )
+        }
+        OPENED -> {
+            AppBarSearch(
+                text = text,
+                onClearClicked = {
+                    if (text.isNotEmpty()) {
+                        text = ""
+                    } else {
+                        searchBarState = CLOSED
+                    }
+                },
+                onTextChange = { text = it }
+            )
+        }
+    }
+}
+
 
 /**
  *  Scaffold의 TopBar 영역에 삽입할 컴포넌트
@@ -22,9 +53,9 @@ import com.example.linkit.ui.theme.LinkItTheme
  *   - 탐색화면: 돋보기와 정렬 아이콘
  */
 @Composable
-fun AppBar(
+fun AppBarHomeDefault(
     modifier: Modifier = Modifier,
-    isMainScreen: Boolean = true
+    onSearchClicked: () -> Unit
 ) {
     TopAppBar(
         modifier = modifier
@@ -32,8 +63,9 @@ fun AppBar(
         title = {
             Icon(
                 modifier = Modifier
-                    .padding(start=5.dp)
-                    .size(UIConstants.ICON_SIZE),
+                    .padding(start = 5.dp)
+                    .size(UIConstants.ICON_SIZE)
+                    .clickable { onSearchClicked() },
                 imageVector = Icons.Filled.Search,
                 contentDescription = null,
                 tint = Color.Black
@@ -42,7 +74,7 @@ fun AppBar(
         actions = {
             Icon(
                 modifier = Modifier
-                    .padding(end=20.dp)
+                    .padding(end = 20.dp)
                     .size(UIConstants.ICON_SIZE),
                 imageVector = Icons.Filled.AccountCircle,
                 contentDescription = null,
@@ -58,6 +90,8 @@ fun AppBar(
 @Composable
 fun PreviewAppBar() {
     LinkItTheme {
-        AppBar()
+        AppBarHomeDefault(
+            onSearchClicked = { }
+        )
     }
 }
