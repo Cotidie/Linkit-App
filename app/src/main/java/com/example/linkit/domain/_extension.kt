e7888f2ee7c30ac74e237269c2852eb5593230b5
Won Suk <daily142857@gmail.com>
2022-01-30 15:27:37 +0900

    refactor: 익스텐션들을 패키지에 알맞게 이동

--- app/src/main/java/com/example/linkit/domain/_extension.kt
+++ app/src/main/java/com/example/linkit/domain/_extension.kt
@@ -9,7 +9,3 @@ import androidx.compose.ui.platform.LocalContext
 fun String.log() {
     Log.d("디버그", this)
 }
-
-/** Composable 함수 내에서 현재 Context 반환. 축약형으로 쓰기 위함 */
-@Composable
-fun cxt() : Context = LocalContext.current
--- app/src/main/java/com/example/linkit/presentation/_extension.kt
+++ app/src/main/java/com/example/linkit/presentation/_extension.kt
@@ -1,2 +1,18 @@
 package com.example.linkit.presentation
 
+import android.content.Context
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+
+/** Composable 함수 내에서 현재 Context 반환. 축약형으로 쓰기 위함 */
+@Composable
+fun cxt() : Context = LocalContext.current
+
+/** Modifier 관련 익스텐션 */
+@Composable
+fun Modifier.longClick(behavior: () -> Unit) = pointerInput(Unit) {
+    detectTapGestures( onLongPress = {behavior()} )
+}
