0d3e4bed6b587b579d73d209e0cc9be281752344
Won Suk <daily142857@gmail.com>
2022-01-31 11:28:14 +0900

    feat: 편집모드에서 이미지에 체크박스 추가

--- app/src/main/java/com/example/linkit/presentation/component/CardFolder.kt
+++ app/src/main/java/com/example/linkit/presentation/component/CardFolder.kt
@@ -3,34 +3,38 @@ package com.example.linkit.presentation.component
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Group
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.linkit.R
+import com.example.linkit._constant.UIConstants
+import com.example.linkit._enums.UIMode
 import com.example.linkit.domain.interfaces.IFolder
 import com.example.linkit.domain.model.FolderPrivate
+import com.example.linkit.presentation.getBitmap
 import com.example.linkit.presentation.longPress
 import com.example.linkit.ui.theme.LinkItTheme
 
-@ExperimentalFoundationApi
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CardFolder(
     folder: IFolder,
+    uiMode: UIMode,
     onClick: () -> Unit,
     onLongPress: () -> Unit = {}
 ) {
@@ -42,31 +46,29 @@ fun CardFolder(
             )
             .background(Color.Transparent),
     ) {
-        Box(modifier = Modifier.size(110.dp, 110.dp)) {
-            Image(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .clip(RoundedCornerShape(10.dp)),
-                painter = painterResource(id = R.drawable.ic_sample_image_001),
-                contentScale = ContentScale.Crop,
-                contentDescription = folder.name
-            )
-        }
+        ImageWithCheckbox(
+            modifier = Modifier
+                .size(UIConstants.SIZE_IMAGE_FOLDER),
+            uiMode = uiMode,
+            image = folder.image!!.asImageBitmap()
+        )
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .background(Color.Transparent)
                 .padding(vertical = 10.dp)
         ) {
+            // 폴더명
             Text(
                 modifier = Modifier
                     .fillMaxHeight()
-                    .padding(start=10.dp),
+                    .padding(start = 10.dp),
                 text = folder.name,
                 color = Color.White,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
             )
+            // 공유폴더 표시 아이콘
             if (!folder.isShared()) {
                 Icon(
                     modifier = Modifier.scale(0.6f),
@@ -75,6 +77,7 @@ fun CardFolder(
                     tint = Color.White
                 )
             }
+            // 더보기 아이콘
             Box(modifier = Modifier.fillMaxSize()) {
                 Icon(
                     modifier = Modifier
@@ -89,34 +92,46 @@ fun CardFolder(
     }
 }
 
+@Composable
+fun ImageWithCheckbox(
+    modifier: Modifier = Modifier,
+    uiMode: UIMode,
+    image: ImageBitmap
+) {
+    var checked by remember { mutableStateOf(false) }
+
+    Box(modifier = modifier) {
+        Image(
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(10.dp)),
+            bitmap = image,
+            contentScale = ContentScale.Crop,
+            contentDescription = null
+        )
+        if (uiMode.isEditMode()) {
+            Checkbox(
+                modifier = Modifier
+                    .align(Alignment.TopStart),
+                checked = checked,
+                onCheckedChange = { checked = !checked },
+                colors = CheckboxDefaults.colors(Color.Black)
+            )
+        }
+    }
+}
+
 @Preview
 @Composable
 fun PreviewFolderCard() {
+    val folder = FolderPrivate(
+        id = 5,
+        name = "개인폴더",
+        image = getBitmap(id = R.drawable.ic_sample_image_001)
+    )
+    val uiMode = UIMode.EDIT_FOLDER
+
     LinkItTheme {
-        Column(
-            modifier = Modifier
-                .clickable { }
-                .background(Color.Transparent),
-        ) {
-            Box(modifier = Modifier.size(150.dp)) {
-                Image(
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(10.dp))
-                        .fillMaxHeight()
-                        .fillMaxWidth(),
-                    painter = painterResource(id = R.drawable.ic_sample_image_001),
-                    contentScale = ContentScale.Crop,
-                    contentDescription = "aa"
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(20.dp)
-                    .background(Color.Transparent)
-            ) {
-                Text("aa", color = Color.White)
-            }
-        }
+        CardFolder(folder, uiMode, {}, {})
     }
 }
--- app/src/main/java/com/example/linkit/presentation/component/GridFolder.kt
+++ app/src/main/java/com/example/linkit/presentation/component/GridFolder.kt
@@ -8,12 +8,14 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.linkit._enums.UIMode
 import com.example.linkit.domain.interfaces.IFolder
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun FolderGrid(
     modifier: Modifier = Modifier,
+    uiMode: UIMode,
     folders: List<IFolder>,
     cells: Int,
     onClick: (item: IFolder) -> Unit,
@@ -28,6 +30,7 @@ fun FolderGrid(
         items(folders) { folder ->
             CardFolder(
                 folder = folder,
+                uiMode = uiMode,
                 onClick = { onClick(folder) },
                 onLongPress = { onLongPress() }
             )
