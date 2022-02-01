580e64c7dab59055f235ff9c7b6a3c347b48fc89
Won Suk <daily142857@gmail.com>
2022-02-01 22:19:36 +0900

    feat: 다이얼로그 용 모델 추가

---
+++ app/src/main/java/com/example/linkit/presentation/model/Dialog.kt
@@ -0,0 +1,13 @@
+package com.example.linkit.presentation.model
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
+class Dialog {
+    var _show: MutableState<Boolean> = mutableStateOf(false)
+    val show: Boolean
+        get() = _show.value
+
+    fun show() { _show.value = true }
+    fun dismiss() { _show.value = false }
+}
