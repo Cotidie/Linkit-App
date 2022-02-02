69b4ef56cb3df42e24d987a0c5f6c74d1875475c
Won Suk <daily142857@gmail.com>
2022-02-02 21:03:55 +0900

    feat: Google OAuth Client 빌더 추가

--- app/src/main/java/com/example/linkit/presentation/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/linkit/presentation/viewmodel/LoginViewModel.kt
@@ -1,8 +1,14 @@
 package com.example.linkit.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.linkit.LinkItApp
+import com.example.linkit.MainActivity
+import com.example.linkit.R
 import com.example.linkit.data.repository.UserRepository
 import com.example.linkit.domain.model.User
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -13,4 +19,18 @@ class LoginViewModel @Inject constructor(
     val currentUser: User = userRepo.getLoggedInUser()
 
     fun isLoggedIn() : Boolean = !currentUser.isGuest()
+
+    private fun getGoogleLoginAuth(): GoogleSignInClient {
+        val cxt = LinkItApp.cxt()
+        val clientId = cxt.getString(R.string.google_client_ID)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestIdToken(clientId)
+            .requestId()
+            .requestProfile()
+            .build()
+
+        return GoogleSignIn.getClient(cxt, gso)
+    }
 }
