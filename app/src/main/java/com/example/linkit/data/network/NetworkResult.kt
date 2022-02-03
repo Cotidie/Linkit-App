baf22f6f153545017401832f8c0dbdc6295f31d6
Won Suk <daily142857@gmail.com>
2022-02-04 05:27:17 +0900

    feat: 네트워크 결과에 로딩 상태 추가

--- app/src/main/java/com/example/linkit/data/network/NetworkResult.kt
+++ app/src/main/java/com/example/linkit/data/network/NetworkResult.kt
@@ -3,3 +3,4 @@ package com.example.linkit.data.network
 sealed class NetworkResult<out T>
 data class Success<out T>(val data: T) : NetworkResult<T>()
 data class Failure(val errorMessage: String?) : NetworkResult<Nothing>()
+data class Loading<out T>(val data: T? = null) : NetworkResult<T>()
