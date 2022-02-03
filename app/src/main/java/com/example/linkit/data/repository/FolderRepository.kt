b9bbeb580968653beb11bded0e977cae98d333de
Won Suk <daily142857@gmail.com>
2022-02-04 05:56:45 +0900

    refactor: 양방향 DTO 적용

--- app/src/main/java/com/example/linkit/data/repository/FolderRepository.kt
+++ app/src/main/java/com/example/linkit/data/repository/FolderRepository.kt
@@ -2,6 +2,7 @@ package com.example.linkit.data.repository
 
 import com.example.linkit.data.room.dao.FolderDao
 import com.example.linkit.data.room.dto.FolderListMapper
+import com.example.linkit.data.room.dto.FolderMapper
 import com.example.linkit.data.room.entity.FolderEntity
 import com.example.linkit.domain.interfaces.IFolder
 import kotlinx.coroutines.Dispatchers
@@ -11,14 +12,15 @@ import javax.inject.Inject
 /** 도메인 모델 Folder, SharedFolder를 담당하는 Repository */
 class FolderRepository @Inject constructor(
     private val folderDao: FolderDao,
-    private val folderMapper: FolderListMapper
+    private val folderMapper: FolderMapper,
+    private val folderListMapper: FolderListMapper
 ){
     fun getAllFolders() : Flow<List<IFolder>>
-        = folderDao.getAllFolders().flowOn(Dispatchers.IO).conflate().map { folderMapper.map(it) }
+        = folderDao.getAllFolders().flowOn(Dispatchers.IO).conflate().map { folderListMapper.mapTo(it) }
 
     suspend fun getFolder(id: Long) : FolderEntity = folderDao.getFolderById(id)
-    suspend fun insert(folderEntity: FolderEntity) = folderDao.insert(folderEntity)
-    suspend fun update(folderEntity: FolderEntity) = folderDao.update(folderEntity)
-    suspend fun delete(folderEntity: FolderEntity) = folderDao.delete(folderEntity)
+    suspend fun insert(folder: IFolder) = folderDao.insert(folderMapper.mapFrom(folder))
+    suspend fun update(folder: IFolder) = folderDao.update(folderMapper.mapFrom(folder))
+    suspend fun delete(folder: IFolder) = folderDao.delete(folderMapper.mapFrom(folder))
     suspend fun deleteAll() = folderDao.deleteAll()
 }
