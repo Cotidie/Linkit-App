0e406a03c440439df6eced84493c56a8274977af
Won Suk <daily142857@gmail.com>
2022-02-04 05:56:16 +0900

    refactor: Folder 객체 id에 기본값 적용

--- app/src/main/java/com/example/linkit/domain/model/FolderShared.kt
+++ app/src/main/java/com/example/linkit/domain/model/FolderShared.kt
@@ -5,7 +5,7 @@ import com.example.linkit.domain.interfaces.IFolder
 import java.util.*
 
 class FolderShared(
-    override val id: Long,
+    override val id: Long = 0,
     override var name: String,
     override var image: Bitmap = EMPTY_BITMAP,
     override val created: Date = Date(),
