ad798e7f8470ad195119b193bbe22885820e8880
Won Suk <daily142857@gmail.com>
2022-02-04 17:25:02 +0900

    커밋2

--- app/src/main/java/com/example/linkit/data/storage/FIleManager.kt
+++ app/src/main/java/com/example/linkit/data/storage/FIleManager.kt
@@ -1,4 +1,10 @@
 package com.example.linkit.data.storage
 
-class FIleManager {
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/** 로컬 파일 이동, 삭제, 저장 등을 수행하는 클래스 */
+@Singleton
+class FIleManager @Inject constructor() {
+    
 }
