3c38f884cac0420f66f1d3d6180bc6adfe8a39c5
Won Suk <daily142857@gmail.com>
2022-02-05 17:22:47 +0900

    feat: Uri -> Bitmap 익스텐션 추가

--- app/src/main/java/com/example/linkit/domain/_extension.kt
+++ app/src/main/java/com/example/linkit/domain/_extension.kt
@@ -3,13 +3,30 @@ package com.example.linkit.domain.model
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.example.linkit.LinkItApp
 
 /** '디버그' 태그를 앞에 자동을 붙여준다. */
 fun String.log() {
     Log.d("디버그", this)
+}
+
+/** Uri -> Bitmap */
+fun Uri.toBitmap() : Bitmap {
+    val appContext = LinkItApp.cxt()
+
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        ImageDecoder.decodeBitmap(ImageDecoder.createSource(appContext.contentResolver, this))
+    } else {
+        MediaStore.Images.Media.getBitmap(appContext.contentResolver, this)
+    }
 }
 
 /** 빈 비트맵 */
