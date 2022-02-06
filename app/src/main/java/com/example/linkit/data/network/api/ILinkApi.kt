ed15c3b55e334042b27356cfd79b95a3d8153044
Won Suk <daily142857@gmail.com>
2022-02-06 19:13:58 +0900

    feat: 단순 URL Fetcher 메소드 정의

--- app/src/main/java/com/example/linkit/data/network/api/ILinkApi.kt
+++ app/src/main/java/com/example/linkit/data/network/api/ILinkApi.kt
@@ -1,9 +1,15 @@
 package com.example.linkit.data.network.api
 
 import com.example.linkit.data.network.model.response.UserResponse
+import okhttp3.ResponseBody
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface ILinkApi {
     @GET
     suspend fun getUserData() : List<UserResponse>
+
+    @GET
+    suspend fun fetchUrl(@Url urlString: String) : Response<ResponseBody>
 }
