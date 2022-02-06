c53e268bb1beb210b974b888f6f991f44eb992fb
Won Suk <daily142857@gmail.com>
2022-02-06 19:22:18 +0900

    feat: Link가 속한 부모 폴더 id를 멤버로 정의

--- app/src/main/java/com/example/linkit/domain/interfaces/ILink.kt
+++ app/src/main/java/com/example/linkit/domain/interfaces/ILink.kt
@@ -5,6 +5,7 @@ import com.example.linkit.domain.model.Url
 
 interface ILink : IFile{
     val id: Long
+    var parentFolder: Long // 링크는 어느 폴더에 속해야 한다.
     override var name: String
     var memo: String
     var url: Url
