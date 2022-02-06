11cd4cfa8a742d8662235918d24a881b90e16a14
Won Suk <daily142857@gmail.com>
2022-02-06 19:19:53 +0900

    feat: favicon Fetch 용 메소드 추가

--- app/src/main/java/com/example/linkit/domain/model/Url.kt
+++ app/src/main/java/com/example/linkit/domain/model/Url.kt
@@ -1,6 +1,13 @@
 package com.example.linkit.domain.model
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.util.Patterns
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
 import java.net.MalformedURLException
 import java.net.URL
 
@@ -35,15 +42,38 @@ class Url constructor() {
 
     fun getString(showProtocol: Boolean = false) : String {
         if (!isValid()) return ""
-        if (showProtocol) return detachProtocol(_url!!)
-        return _url.toString()
+
+        var urlString = _url.toString()
+        if (!showProtocol) urlString = detachProtocol(_url!!)
+
+        return urlString
+    }
+
+    fun getDomain(showProtocol: Boolean = false) : String {
+        if (showProtocol)
+            return "${_url!!.protocol}://${attachWww(_url!!.host)}"
+
+        return attachWww(_url!!.host)
+    }
+
+    fun getFaviconUrl(size: Int = 128) : String {
+//        return "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${getDomain()}&size=$size"
+        return "https://www.google.com/s2/favicons?sz=$size&domain_url=${getDomain(true)}"
     }
 
     private fun attachHttp(url: String) : String {
-        if (!url.startsWith("http://") || !url.startsWith("https://"))
-            return "http://$url"
+        if (!url.startsWith("http://") && !url.startsWith("https://"))
+            return "https://$url"
 
         return url
+    }
+
+    /** www가 없는 도메인에 www를 붙인다.*/
+    private fun attachWww(domain: String) : String {
+        if (!domain.startsWith("www."))
+            return "www.$domain"
+
+        return domain
     }
 
     private fun detachProtocol(url: URL) : String {
