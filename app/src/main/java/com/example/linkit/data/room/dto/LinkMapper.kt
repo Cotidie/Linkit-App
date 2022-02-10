03dc6e05daac11f5389a6923d4f8923f77ef2f51
Won Suk <daily142857@gmail.com>
2022-02-11 00:42:28 +0900

    fix: 링크 id가 제대로 변환되지 않던 문제 수정

--- app/src/main/java/com/example/linkit/data/room/dto/LinkMapper.kt
+++ app/src/main/java/com/example/linkit/data/room/dto/LinkMapper.kt
@@ -14,6 +14,7 @@ import javax.inject.Singleton
 class LinkToEntity @Inject constructor() : Mapper<ILink, LinkWithTags> {
     override fun map(input: ILink): LinkWithTags {
         val link =  LinkEntity(
+            id = input.id,
             folderId = input.parentFolder,
             url = input.url.getString(true),
             image = input.image
