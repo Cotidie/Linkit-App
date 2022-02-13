f84b6c9e27a595884f83d2afa19e3aa0ec345acf
Won Suk <daily142857@gmail.com>
2022-02-14 00:57:37 +0900

    refactor: 커스텀 칩 사용성 개선

--- app/src/main/java/com/example/linkit/presentation/component/ChipCustom.kt
+++ app/src/main/java/com/example/linkit/presentation/component/ChipCustom.kt
@@ -2,9 +2,13 @@ package com.example.linkit.presentation.component
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -17,35 +21,41 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 /** TODO: modifier는 하나만 두고, 배경색, 패딩 등 해당하는 값을 직접 받기 */
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun CustomChip(
     text: String,
     prefix: String = "",
+    padding: PaddingValues = PaddingValues(horizontal = 6.dp),
     borderWidth: Dp = (-1).dp,
     borderColor: Color = Color.Black,
     backgroundColor: Color = Color.Transparent,
     onClick: () -> Unit = {},
 ) {
     Surface(
-        modifier = Modifier.clickable { onClick() },
+        onClick = onClick,
         shape = CircleShape,
         color = backgroundColor,
         border = BorderStroke(borderWidth, borderColor)
     ) {
-        Text(text = prefix)
+        Row(Modifier.padding(padding)) {
+            Text(
+                text = prefix,
+                style = MaterialTheme.typography.subtitle1
+            )
 
-        Text(
-            modifier = Modifier.padding(horizontal=10.dp),
-            text = text,
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.subtitle1
-        )
+            Text(
+                text = text,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.subtitle1
+            )
+        }
     }
 }
 
 @Preview
 @Composable
 fun PreviewTagChip() {
-    CustomChip(text = "# 테스트")
+    CustomChip(prefix = "#", text = "테스트", borderWidth = 1.dp)
 }
 
