59e8752287f61332d58b229a46dc02623c696f47
Won Suk <daily142857@gmail.com>
2022-02-16 00:02:49 +0900

    refactor: Presentation 모델 분리 및 적용

--- app/src/main/java/com/example/linkit/presentation/viewmodel/ContentViewModel.kt
+++ app/src/main/java/com/example/linkit/presentation/viewmodel/ContentViewModel.kt
@@ -5,10 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.linkit._enums.UIMode
-import com.example.linkit.domain.repository.LinkRepository
-import com.example.linkit.domain.interfaces.ILink
-import com.example.linkit.domain.model.Link
 import com.example.linkit.domain.model.log
+import com.example.linkit.domain.repository.LinkRepository
+import com.example.linkit.presentation.mapper.LinkMapper
+import com.example.linkit.presentation.model.LinkView
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,23 +16,26 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ContentViewModel @Inject constructor(
-    private val linkRepo: LinkRepository
+    private val linkRepo: LinkRepository,
+    private val linkMapper: LinkMapper
 ): ViewModel() {
-    private val _link = mutableStateOf(ILink.EMPTY)
-    val link: State<ILink> = _link
+    private var _link = mutableStateOf(LinkView())
+    val link: State<LinkView> = _link
     var uiMode = mutableStateOf(UIMode.NORMAL)
 
     /** DB에서 화면에 표시할 링크를 불러온다. */
     fun setLink(id: Long) {
         viewModelScope.launch(Dispatchers.IO) {
-            _link.value = linkRepo.getLink(id)
+            val domain = linkRepo.getLink(id)
+            _link.value = linkMapper.map(domain)
         }
     }
 
     /** 현재 링크를 DB에 반영한다. */
     fun saveLink() {
         viewModelScope.launch(Dispatchers.IO) {
-            linkRepo.updateLink(_link.value)
+            val domain = linkMapper.map(_link.value)
+            linkRepo.updateLink(domain)
         }
     }
 
