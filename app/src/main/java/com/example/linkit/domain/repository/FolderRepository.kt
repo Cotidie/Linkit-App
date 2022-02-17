d11ca5a01df456b8e257780686c6445ad0549e27
Won Suk <daily142857@gmail.com>
2022-02-18 02:44:23 +0900

    feat: 폴더 삭제 구현

--- app/src/main/java/com/example/linkit/domain/repository/FolderRepository.kt
+++ app/src/main/java/com/example/linkit/domain/repository/FolderRepository.kt
@@ -2,7 +2,10 @@ package com.example.linkit.domain.repository
 
 import com.example.linkit.domain.repository.mapper.FolderMappers
 import com.example.linkit.data.room.dao.FolderDao
+import com.example.linkit.data.room.dao.LinkDao
 import com.example.linkit.domain.interfaces.IFolder
+import com.example.linkit.domain.model.log
+import com.example.linkit.domain.repository.mapper.LinkMappers
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import javax.inject.Inject
@@ -10,18 +13,25 @@ import javax.inject.Inject
 /** 도메인 모델 [FolderPrivate], [FolderShared]를 담당하는 Repository */
 class FolderRepository @Inject constructor(
     private val folderDao: FolderDao,
-    private val folderDto: FolderMappers
+    private val linkDao: LinkDao,
+    private val folderMapper: FolderMappers,
 ){
     fun getAllFolders() : Flow<List<IFolder>> {
         return folderDao.getAllFolders()
             .flowOn(Dispatchers.IO)
             .conflate()
-            .map { folderDto.map(it) }
+            .map { folderMapper.map(it) }
     }
 
-    suspend fun getFolder(id: Long) : IFolder = folderDao.getFolderById(id).run { folderDto.map(this) }
-    suspend fun insert(folder: IFolder) = folderDao.insert(folderDto.map(folder))
-    suspend fun update(folder: IFolder) = folderDao.update(folderDto.map(folder))
-    suspend fun delete(folder: IFolder) = folderDao.delete(folderDto.map(folder))
+    suspend fun getFolder(id: Long) : IFolder = folderDao.getFolderById(id).run { folderMapper.map(this) }
+    suspend fun insert(folder: IFolder) = folderDao.insert(folderMapper.map(folder))
+    suspend fun update(folder: IFolder) = folderDao.update(folderMapper.map(folder))
+
+    /** 폴더와 폴더 내의 링크를 모두 삭제한다. */
+    suspend fun delete(folder: IFolder) {
+        val links = linkDao.getLinksInFolder(folderId = folder.id).first()
+        for (link in links) linkDao.delete(link)
+        folderDao.delete(folderMapper.map(folder))
+    }
     suspend fun deleteAll() = folderDao.deleteAll()
 }
