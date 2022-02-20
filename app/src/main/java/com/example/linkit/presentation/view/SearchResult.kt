1abe72cb08207a1decc498363242ec2fb32c1b84
Won Suk <daily142857@gmail.com>
2022-02-21 00:23:14 +0900

    feat: 검색화면에서 텍스트 수정 가능

--- app/src/main/java/com/example/linkit/presentation/view/SearchResult.kt
+++ app/src/main/java/com/example/linkit/presentation/view/SearchResult.kt
@@ -8,10 +8,7 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,6 +17,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.linkit._constant.UIConstants
 import com.example.linkit._enums.UIMode
 import com.example.linkit.domain.interfaces.ILink
 import com.example.linkit.domain.model.Link
@@ -37,6 +35,7 @@ fun SearchResultScreen(
 ) {
     val viewModel = hiltViewModel<SearchViewModel>()
     val scrollState = rememberLazyListState()
+    var searchText by remember { mutableStateOf(searchUrl) }
 
     LaunchedEffect(Unit) {
         viewModel.collectLinks(searchUrl, folderId)
@@ -45,10 +44,10 @@ fun SearchResultScreen(
     Scaffold(
         topBar = {
             AppBarSearch(
-                modifier = Modifier.height(80.dp),
-                text = "샘플",
-                onClearClicked = {},
-                onTextChange = {}
+                modifier = Modifier.height(UIConstants.HEIGHT_APP_BAR),
+                text = searchText,
+                onClearClicked = { searchText = "" },
+                onTextChange = { searchText = it }
             )
         }
     ){ innerPadding ->
