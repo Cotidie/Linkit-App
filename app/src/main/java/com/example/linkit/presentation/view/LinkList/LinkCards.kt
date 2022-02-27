72a2e4536e435e85467e544d661d508c4cda3cf3
Won Suk <daily142857@gmail.com>
2022-02-28 04:37:58 +0900

    refactor: 컨벤션에 따른 리팩토링
    Background (배경색 설정)
    Content (여백 설정)

---
+++ app/src/main/java/com/example/linkit/presentation/view/LinkList/LinkCards.kt
@@ -0,0 +1,45 @@
+package com.example.linkit.presentation.view.LinkList
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.linkit._enums.UIMode
+import com.example.linkit.presentation.component.CardLink
+import com.example.linkit.presentation.model.LinkView
+import com.example.linkit.presentation.navigation.Screen
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
+@Composable
+fun LinkCards(
+    navController: NavController,
+    links: List<LinkView>,
+    scrollState: LazyListState = rememberLazyListState(),
+) {
+    LazyColumn(
+        state = scrollState
+    ) {
+        items(links) { link ->
+            Spacer(Modifier.height(20.dp))
+            CardLink(
+                modifier = Modifier.height(80.dp),
+                link = link,
+                onIconClick = {
+                    navController.navigate(
+                        route = Screen.Content.route.plus("/${link.id}")
+                    )
+                },
+                uiMode = UIMode.NORMAL
+            )
+        }
+    }
+}
