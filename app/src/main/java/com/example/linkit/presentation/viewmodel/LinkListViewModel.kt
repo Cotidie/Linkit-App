6d434c65fdcc93094d45358fb2e88f34f978509d
Won Suk <daily142857@gmail.com>
2022-02-28 21:26:02 +0900

    feat: LinkList 전용 뷰모델에 검색 기능 구현

--- app/src/main/java/com/example/linkit/presentation/viewmodel/LinkListViewModel.kt
+++ app/src/main/java/com/example/linkit/presentation/viewmodel/LinkListViewModel.kt
@@ -3,10 +3,12 @@ package com.example.linkit.presentation.viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.linkit._enums.SearchType
 import com.example.linkit._enums.SortingOption
 import com.example.linkit._enums.UIMode
 import com.example.linkit.domain.interfaces.IFolder
 import com.example.linkit.domain.interfaces.ILink
+import com.example.linkit.domain.model.log
 import com.example.linkit.domain.repository.LinkRepository
 import com.example.linkit.presentation.mapper.LinkMapper
 import com.example.linkit.presentation.model.LinkView
@@ -24,11 +26,47 @@ class LinkListViewModel @Inject constructor(
     private val linkRepo: LinkRepository,
     private val linkMapper: LinkMapper
 ) : ViewModel() {
-    val links = mutableStateOf(emptyList<LinkView>())
+    private val allLinks = mutableStateOf(emptyList<LinkView>())
+    val displayLinks = mutableStateOf(emptyList<LinkView>())
     val uiMode = mutableStateOf(UIMode.ALL_LINK)
     val sortBy = mutableStateOf(SortingOption.OLDEST)
+    val searchBy = mutableStateOf(SearchType.URL)
+    val searchText = mutableStateOf("")
 
     init { collectLinks() }
+
+    fun searchLinks() {
+        val textToSearch = searchText.value
+        if (textToSearch.isBlank()) {
+            displayLinks.value = allLinks.value; "값이 비어있음: ${searchText.value}".log()
+            return
+        }
+
+        val filtered = filterBySearchType(allLinks.value)
+        val sorted = sortBySortingOption(filtered)
+
+        displayLinks.value = sorted
+    }
+
+    private fun sortBySortingOption(target: List<LinkView>): List<LinkView> {
+        return when (sortBy.value) {
+            SortingOption.OLDEST -> target.sortedBy { it.created.value }
+            SortingOption.NEWEST -> target.sortedByDescending { it.created.value }
+        }
+    }
+
+    private fun filterBySearchType(target: List<LinkView>): List<LinkView> {
+        return target.filter { containsBySearchType(linkView = it) }
+    }
+
+    private fun containsBySearchType(linkView: LinkView): Boolean {
+        val textToSearch = searchText.value
+
+        return when (searchBy.value) {
+            SearchType.URL -> linkView.containsInUrl(textToSearch)
+            SearchType.TAG -> linkView.containsInTags(textToSearch)
+        }
+    }
 
     private fun collectLinks() {
         viewModelScope.launch(Dispatchers.IO) {
@@ -36,7 +74,7 @@ class LinkListViewModel @Inject constructor(
                 .distinctUntilChanged()
                 .map { linkMapper.map(it) }
                 .collect {
-                    links.value = it
+                    allLinks.value = it
                 }
         }
     }
